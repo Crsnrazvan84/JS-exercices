9d29169cc1d504aa4731b028adf1ed5c7cd45537
Crisan Razvan <crsn_razvan@yahoo.com>
2019-01-09 17:26:58 +0200

    added aray sort exercises

--- Learn-JS/arrays.js
+++ Learn-JS/arrays.js
@@ -1,47 +1,103 @@
 
-//given array
-var x = [1001, 8, 111, 56]
+//given FIXED array
+var x = [101, 64, 23, 56]
 
-// find the biggest number in the given array
+//given DYNAMIC array
 
-function biggestNr (){
+var y = [4, 6, 8, 10, 12]
+
+// find the biggest number in the given fixed array
+
+function biggestNrFixed (){
     var a = x[0] > x[1] ? x[0] : x[1];
     var b = a > x[2] ? a : x[2];
     var c = b > x[3] ? b : x[3];
     console.info('The biggest number in this array is ', c);
     
 }
-biggestNr();
+// biggestNrFixed();
 
-// find the biggest number in the given array
+// find the biggest number in the given dynamic array
+function sortAscendingDynamic(){
+    var sortaD = y.sort(function(a, b){
+        return b-a
+    })
 
-function smallestNr (){
+    console.log('the biggest number is ' , sortaD[0])
+}
+
+// sortAscendingDynamic();
+
+
+// find the smallest number in the given fixed array
+
+function smallestNrFixed (){
     var a = x[0] < x[1] ? x[0] : x[1];
     var b = a < x[2] ? a : x[2];
     var c = b < x[3] ? b : x[3]
     console.info('The smallest number in this array is ', c);
 }
 
-smallestNr();
+// smallestNrFixed();
 
-//sort ascending
+//find the smallest nnumber in the given dynamic array
+
+function sortDescendingDynamic(){
+    var sortdD = y.sort(function(a, b){
+        return a-b
+    })
+
+    console.log('the smallest number is ' , sortdD[0])
+}
+
+// sortDescendingDynamic();
+
+//TODO sort ascending fixed
+
+function sortAscendingFixed(){
+    var a = x[0];
+    var b = x[1];
+    var c = x[2];
+    var d = x[3];
+
+   if (x[0] > x[1]){
+       x[0] = b;
+       x[1] = a;
+       
+   }
+  
+
+   if(x[1] > x[2]){
+       x[1] = c;
+       x[2] = b;
+   }
+    console.log('x0 is ', x[0]);
+    console.log('x1 is ', x[1]);
+    console.log('x2 is ', x[2]);
+    
+    // console.log('Sort ascending fixed array: ', x)
+}
+sortAscendingFixed();
+
+
+//sort ascending dynamic
 
 function sortAscending(){
-    var sorta = x.sort(
+    var sorta = y.sort(
         function(a, b){
             return a-b
         }
     )
     console.log('Sort ascending: ', sorta)
 }
-sortAscending();
+// sortAscending();
 
-//sort descending
+//sort descending dynamic
 
 function sortDescending(){
-    var sortd = x.sort(function(a, b){
+    var sortd = y.sort(function(a, b){
         return b-a
     })
     console.log('Sort descending ', sortd)
 }
-sortDescending();
+// sortDescending();
