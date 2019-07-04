a3c8a206e141b1057a16eba4d3863e34223bcbcf
Razvan Crisan <crsn_razvan@yahoo.com>
2019-07-04 12:26:20 +0300

    change DOM Elements

--- Complete JavaScript course/08. DOM/4. Change DOM elements - Please start here/code.js
+++ Complete JavaScript course/08. DOM/4. Change DOM elements - Please start here/code.js
@@ -1,52 +1,32 @@
-//change HTML content
+//change html content
 var p1 = document.getElementById("p1");
 p1.innerHTML = "Javascript is fun!";
 var p2 = document.getElementById("p2");
-p2.innerHTML += " This is number 2."
+p2.innerHTML += " This is nr 2";
 var p3 = document.getElementById("p3");
-p3.textContent = "Some new text!";
+p3.textContent = "Some new text";
 var link = document.getElementById("link");
 link.setAttribute("href", "http://wikipedia.org");
 
 //change HTML styling
 p1.style.backgroundColor = "pink";
-//p2.style.display = "none";
+p2.style.display = "none";
 p3.style.color = "orange";
 p3.style.position = "fixed";
 p3.style.top = "100px";
 p3.style.left = "300px";
 
-//using the forEach method, add a new brand to each list
-var ul = document.getElementsByTagName("ul");//careful, this is an HTML collection not an array.
-console.log(ul);
-console.log(ul.length);
-
-//add forEach to the prototype of HTMLCollection
+//using thr forEach, add a new make for each list
+var lists = document.getElementsByTagName("ul");
 HTMLCollection.prototype.forEach = Array.prototype.forEach;
-ul.forEach(function(element){
-    element.innerHTML += "<li>Ford</li>";
+lists.forEach(list => {
+  var newLi = document.createElement("li");
+  var ford = document.createTextNode("Ford");
+  var newCar = newLi.appendChild(ford);
+  list.appendChild(newCar);
 });
 
 var green = document.getElementsByClassName("green");
 green.forEach(function(element){
-    element.style.color = "blue";
-});
-
-//remove a child element
-var list1 = document.getElementById("list1");
-var item1 = document.getElementById("item1");
-list1.removeChild(item1);
-
-//build a method remove which removes any element from the DOM
-Element.prototype.remove = function(){
-    var parent = this.parentElement;
-    parent.removeChild(this);
-//    this.parentElement.removeChild(this);
-}
-
-list1.remove();
-
-
-
-
-
+    element.style.color = 'blue';
+})
