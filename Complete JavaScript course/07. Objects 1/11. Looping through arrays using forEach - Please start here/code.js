d0c23df4c1253e24a84658c8f6e96878e568a0ba
Razvan Crisan <crsn_razvan@yahoo.com>
2019-07-04 10:13:18 +0300

    for each in arrays

--- Complete JavaScript course/07. Objects 1/11. Looping through arrays using forEach - Please start here/code.js
+++ Complete JavaScript course/07. Objects 1/11. Looping through arrays using forEach - Please start here/code.js
@@ -1,2 +1,12 @@
 var shoppingList = ['orange', 'apple', 'banana'];
 console.log(shoppingList);
+
+shoppingList.forEach(function(element){
+    console.log(element);
+});
+
+//ES6
+
+for(element of shoppingList){
+    console.log(element);
+}
