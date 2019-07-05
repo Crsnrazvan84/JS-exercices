4b241151040a3536f036bd7a112358a8b8ee0efa
Razvan Crisan <crsn_razvan@yahoo.com>
2019-07-05 10:37:30 +0300

    remove DOM elements

--- Complete JavaScript course/08. DOM/4. Change DOM elements - Please start here/code.js
+++ Complete JavaScript course/08. DOM/4. Change DOM elements - Please start here/code.js
@@ -19,14 +19,21 @@ p3.style.left = "300px";
 //using thr forEach, add a new make for each list
 var lists = document.getElementsByTagName("ul");
 HTMLCollection.prototype.forEach = Array.prototype.forEach;
-lists.forEach(list => {
-  var newLi = document.createElement("li");
-  var ford = document.createTextNode("Ford");
-  var newCar = newLi.appendChild(ford);
-  list.appendChild(newCar);
-});
+lists.forEach(list => (list.innerHTML += `<li>Ford</li>`));
 
 var green = document.getElementsByClassName("green");
-green.forEach(function(element){
-    element.style.color = 'blue';
-})
+green.forEach(function(element) {
+  element.style.color = "blue";
+});
+
+//remove a child element
+var list1 = document.getElementById("list1");
+var idem1 = document.getElementById("item1");
+list1.removeChild(item1);
+
+//build a method remove which removes any element from the DOM
+Element.prototype.remove = function() {
+  var parent = this.parentElement;
+  parent.removeChild(this);
+};
+list1.remove();
