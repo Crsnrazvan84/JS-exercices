2d2548447b24b48f74d2508fa8625bc881bbe6a7
Razvan Crisan <crsn_razvan@yahoo.com>
2019-07-08 11:41:53 +0300

    declaration overriding variable declaration

---
+++ Complete JavaScript course/09. Execution Context - Hoisting - This/4. Function declaration overriding variable declaration - Please start here/code.js
@@ -0,0 +1,16 @@
+// var greet = "hello";
+// function greet() {
+//   return "Hi";
+// }
+
+// console.log(typeof greet);
+// greet();
+
+//2
+
+var greet = function(){
+    return "Hi";
+}
+var greet = "hello";
+
+console.log(typeof greet);
