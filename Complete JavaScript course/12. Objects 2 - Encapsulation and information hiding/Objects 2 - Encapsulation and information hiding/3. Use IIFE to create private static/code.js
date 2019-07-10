c28f2c81d519e0b4185539528e392bc3a0b40e2a
Razvan Crisan <crsn_razvan@yahoo.com>
2019-07-10 09:48:13 +0300

    Use IIFE to create private static properties

--- Complete JavaScript course/12. Objects 2 - Encapsulation and information hiding/Objects 2 - Encapsulation and information hiding/3. Use IIFE to create private static/code.js
+++ Complete JavaScript course/12. Objects 2 - Encapsulation and information hiding/Objects 2 - Encapsulation and information hiding/3. Use IIFE to create private static/code.js
@@ -1,53 +1,64 @@
-var phone = function(newMake, newModel, newWarranty){
-    //private properties
-    var make, model, warranty;
+var c = (function() {
+    //private static poperty
+    var numberOfPhones = 0;
     //private method
-    function validWarranty(key){
-        return key == 'password';
+    function validWarranty(key) {
+        return key == "password";
     }
-    
-    
-    //privileged methods have access to private properties and methods
-    this.setMake = function(x){
-        make = x;
-    };
-    this.setModel = function(x){
-        model = x;
-    };
-    this.setWarranty = function(x, key){
-        if(validWarranty(key)){
-            warranty = x;
-        }else{
-            console.log('Wrong key!')
-        }
-    };
-    this.getMake = function(){
-        return make;
-    };
-    this.getModel = function(){
-        return model;
-    };
-    this.getWarranty = function(){
-        return warranty;
-    };
-    this.extendWarranty = function(x, key){
-        if(validWarranty(key)){
-            warranty += x;
-        }else{
-            console.log('Wrong key!')
-        }
-    }
-    
-    //Constructor code
-    this.setMake(newMake);
-    this.setModel(newModel);
-    this.setWarranty(newWarranty, 'password');
-    
-};
+    var phone = function(newMake, newModel, newWarranty) {
+        //private properties
+        var make, model, warranty;
 
-var myPhone = new phone('Samsung', 'Galaxy S6', 12);
+        //privileged methods have access to private properties and methods
+        this.setMake = function(x) {
+            make = x;
+        };
+        this.setModel = function(x) {
+            model = x;
+        };
+        this.setWarranty = function(x, key) {
+            if (validWarranty(key)) {
+                warranty = x;
+            } else {
+                console.log("Wrong key!");
+            }
+        };
+        this.getMake = function() {
+            return make;
+        };
+        this.getModel = function() {
+            return model;
+        };
+        this.getWarranty = function() {
+            return warranty;
+        };
+        this.extendWarranty = function(x, key) {
+            if (validWarranty(key)) {
+                warranty += x;
+            } else {
+                console.log("Wrong key!");
+            }
+        };
+
+        //Constructor code
+        if (numberOfPhones > 2) {
+            throw new Error("Too many phones");
+        } else {
+            numberOfPhones++;
+        }
+        this.setMake(newMake);
+        this.setModel(newModel);
+        this.setWarranty(newWarranty, "password");
+    };
+    return phone;
+})();
+
+var myPhone = new c("Samsung", "Galaxy S6", 12);
+var myPhone2 = new c("Samsung", "Galaxy S6", 12);
+var myPhone3 = new c("Samsung", "Galaxy S6", 12);
+// var myPhone4 = new c("Samsung", "Galaxy S6", 12);
 console.log(myPhone.getWarranty());
-myPhone.setWarranty(24, 'password');
+myPhone.setWarranty(24, "password");
 console.log(myPhone.getWarranty());
 //try this
 myPhone.warranty = 36;
