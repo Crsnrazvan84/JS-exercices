d21971871ac89a9aa9e904de686854c4b346dd46
Razvan Crisan <crsn_razvan@yahoo.com>
2019-07-10 10:49:47 +0300

    Inheritance and the prototype chain - extend

--- Complete JavaScript course/13. Objects 3 - Inheritance and Prototype Chain/6. Inheritance and the prototype chain - extend - Please start here/code.js
+++ Complete JavaScript course/13. Objects 3 - Inheritance and Prototype Chain/6. Inheritance and the prototype chain - extend - Please start here/code.js
@@ -1,17 +1,31 @@
 //superClass
-function phone(make, model, warranty, colour){
+function phone(make, model, warranty, colour) {
     this.make = make;
     this.model = model;
     this.warranty = warranty;
     this.colour = colour;
 }
 phone.prototype = {
-    extendWarranty: function(x){
+    extendWarranty: function(x) {
         this.warranty += x;
     }
 };
 //subClass
-function smartPhone(make, model){
+function smartPhone(make, model) {
     this.make = make;
     this.model = model;
 }
+
+function extend(childClass, parentClass) {
+    childClass.prototype = new parentClass("Samsung", "S6", 12, "grey");
+    childClass.prototype.constructor = childClass;
+}
+
+extend(smartPhone, phone);
+
+var myPhone = new phone();
+var mySmartPhone = new smartPhone('Apple', 'Iphone 6');
+console.log(mySmartPhone);
+console.log(mySmartPhone.warranty);
+mySmartPhone.extendWarranty(12);
+console.log(mySmartPhone.warranty);
