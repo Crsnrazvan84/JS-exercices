576cdb5cc02057fe5347a6910239c51f8c74792a
Razvan Crisan <crsn_razvan@yahoo.com>
2019-07-10 11:11:31 +0300

    Class Augmentation

--- Complete JavaScript course/13. Objects 3 - Inheritance and Prototype Chain/7. Class Augmentation - Please start here/code.js
+++ Complete JavaScript course/13. Objects 3 - Inheritance and Prototype Chain/7. Class Augmentation - Please start here/code.js
@@ -1,17 +1,50 @@
-//superClass
-function phone(make, model, warranty, colour){
+//superClass1
+function phone1(make, model, warranty, colour) {
     this.make = make;
     this.model = model;
     this.warranty = warranty;
     this.colour = colour;
 }
-phone.prototype = {
-    extendWarranty: function(x){
+phone1.prototype = {
+    extendWarranty: function(x) {
         this.warranty += x;
     }
 };
+
+//superClass2
+function phone2(warranty) {
+    this.warranty = warranty;
+    this.extendWarranty = function() {
+        this.warranty = 0;
+    };
+}
+
 //subClass
-function smartPhone(make, model){
+function smartPhone(make, model, warranty) {
     this.make = make;
     this.model = model;
+    this.warranty = warranty;
 }
+
+//augment function
+function augment(receivingClass, givingClass) {
+    var helperObject = new givingClass();
+    var receivingInstance = new receivingClass()
+    for (var i in helperObject) {
+        if(!(receivingInstance.hasOwnProperty(i))) {
+            receivingClass.prototype[i] = helperObject[i];
+        }
+    }
+}
+
+// //property inside object?
+// console.log("extendWarranty" in new phone1());
+
+// //cycle through properties/methods of an object
+// for (var i in new phone1()) {
+//     console.log(i);
+// }
+
+// //hasOwnProperty
+// var myPhone = new phone1();
+// console.log(myPhone.hasOwnProperty("extendWarranty"));
