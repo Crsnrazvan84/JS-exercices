ec08465dc065f0d6d0dadabb9068cff0f1c8f900
Razvan Crisan <crsn_razvan@yahoo.com>
2019-07-11 17:39:21 +0300

    Object Cloning

---
+++ Complete JavaScript course/13. Objects 3 - Inheritance and Prototype Chain/8. Object Cloning - Please start here/code.js
@@ -0,0 +1,26 @@
+//create a function wich will return an empty clone of an object
+
+function clone(object) {
+    var C = function(){};
+    C.prototype = object;
+    return new C();
+}
+
+var phone1 = {
+    make: 'Apple',
+    model: 'Iphone 6',
+    warranty: 12,
+    extendWarranty: function (x) {
+        this.warranty += x;
+    }
+}
+
+var phone2 = clone(phone1);
+console.log(phone2);
+console.log(phone2.make);
+console.log(phone2.extendWarranty(12));
+console.log(phone2.warranty);
+
+for(var i in phone2){
+    console.log(i);
+}
